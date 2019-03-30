2e189dc261bf105399285fa7903fce66e690d234
Joshua Weinstein <jweinstein@splunk.com>
2019-03-29 23:49:52 -0700

    add entry meta, add to receipt.json

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Vocab entry script
+ */
+
+ module.exports = {
+ 	name:'GiveEdu - Vocab',
+ 	description:'A mobile web applicaiton that teaches english vocabulary, for free.',
+ 	author:'GiveEdu - (Written by: Joshua Weinstein)'
+ };
