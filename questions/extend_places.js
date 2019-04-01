f3a455e88b4c6ec303e3dde06927ce294050b12d
Joshua Weinstein <jweinstein@splunk.com>
2019-03-31 23:13:08 -0700

    more questions, 1.0.1

--- questions/extend_places.js
+++ questions/extend_places.js
@@ -7,7 +7,10 @@
      {text:'A ______ works at a bakery.', a:'mailman', b:'chef', c:'artisan', d:'sculptor', cor:'b', lvl:1},
      {text:'I need to catch a ______ at the airport.', a:'ride', b:'boat', c:'flight', d:'plane', cor:'c', lvl:1},
      {text:'A _____ has lots of books.', a:'library', b:'airport', c:'cafe', d:'office', cor:'a', lvl:1},
-     {text:'A waiter works at a ______.', a:'school', b:'library', c:'home', d:'restaurant', cor:'d', lvl:2}
+     {text:'A waiter works at a ______.', a:'school', b:'library', c:'home', d:'restaurant', cor:'d', lvl:2},
+     {text:'A lawyer works at a ______.', a:'house', b:'park', c:'court', d:'restaurant', cor:'c', lvl:2},
+     {text:'A cafe sells _____.', a:'food', b:'clothes', c:'cars', d:'pets', cor:'a', lvl:1},
+     {text:'A toilet is always in the _______.', a:'bedroom', b:'bathroom', c:'kitchen', d:'backyard', cor:'b', lvl:2}
  ];
 
  for(var i_place_ex = 0; i_place_ex < placesQuestionsExtension.length;i_place_ex++) {
